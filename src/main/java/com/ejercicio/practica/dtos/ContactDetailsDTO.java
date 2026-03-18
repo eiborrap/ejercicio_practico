f5319fd73d57bd4e07ace82f6a276aa981b51a03
eiborrap <eiborrap@emeal.nttdata.com>
2026-03-18 12:37:18 +0100

    Added dtos

---
+++ src/main/java/com/ejercicio/practica/dtos/ContactDetailsDTO.java
@@ -0,0 +1,71 @@
+package com.ejercicio.practica.dtos;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+
+public class ContactDetailsDTO implements Serializable{
+    private final Integer telephone;
+    private final String street;
+    private final String email;
+
+    public ContactDetailsDTO(Builder builder){
+        this.telephone = builder.telephone;
+        this.street = builder.street;
+        this.email = builder.email;
+    }
+
+
+
+    public int getTelephone(){
+        return telephone; //It can't be null, telephone is mandatory
+    }
+    public Optional<String> getStreet(){
+        return Optional.ofNullable(street);
+    }
+    public Optional<String> getEmail(){
+        return Optional.ofNullable(email);
+    }
+
+        
+    protected ContactDetailsDTO copy(){
+        return new ContactDetailsDTO.Builder(telephone).street(street).email(email).build();
+    }
+
+    public static final class Builder{
+        private final Integer telephone; //obligatorio
+        private String street;
+        private String email;
+
+        public Builder(Integer telephone){
+            if(telephone == null){
+                throw new IllegalArgumentException("telephone is mandatory");
+            }
+            this.telephone = telephone;
+        }
+        public Builder street(String street){
+            this.street = street;
+            return this;
+        }
+        public Builder email(String email){
+            this.email = email;
+            return this;
+        }
+
+        public ContactDetailsDTO build(){
+            return new ContactDetailsDTO(this);
+        }
+    }
+
+    @Override
+    public String toString(){
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+            .append("telephone",telephone)
+            .append("street",street)
+            .append("email",email)
+            .toString();
+    }
+}
---
+++ src/main/java/com/ejercicio/practica/dtos/PersonaDTO.java
@@ -0,0 +1,117 @@
+package com.ejercicio.practica.dtos;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema
+@JsonInclude
+public class PersonaDTO implements Serializable{
+    private final Integer idPerson;
+    private final String name;
+    private final String fullName;
+    private final String lastName;
+    private final String DNI;
+    private final ContactDetailsDTO contactDetails;
+
+    public PersonaDTO(PersonaDTO.Builder builder){
+        idPerson = builder.idPerson;
+        name = builder.name;
+        lastName = builder.lastName;
+        fullName = builder.fullName;
+        DNI = builder.DNI;
+        contactDetails = builder.contactDetails.copy(); //Copiamos solo aqui, no en el builder
+    }
+
+    public Optional<Integer> getIdPerson() {
+        return Optional.ofNullable(idPerson);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Optional<String> getFullName() {
+        return Optional.ofNullable(fullName);
+    }
+    
+    public String getDNI() {
+        return DNI;
+    }
+
+    public ContactDetailsDTO getContactDetails() {
+        return contactDetails;
+    }
+
+    public static final class Builder{
+        private Integer idPerson;
+        private String name;
+        private String lastName;
+        private String fullName;
+        private String DNI;
+        private ContactDetailsDTO contactDetails;
+
+        //Se piden todos los parámetros obligatorios (En este caso todos) 
+        // para no crear un objeto incompleto
+        public Builder(
+            String name, 
+            String lastName, 
+            String DNI, 
+            ContactDetailsDTO contactDetails
+        ){
+            if(name == null || StringUtils.isBlank(name)){
+                throw new IllegalArgumentException("name is mandatory");
+            }
+            if(lastName == null || StringUtils.isBlank(lastName)){
+                throw new IllegalArgumentException("lastName is mandatory");
+            }
+            if(DNI == null || StringUtils.isBlank(DNI)){
+                throw new IllegalArgumentException("DNI is mandatory");
+            }
+            if(contactDetails == null){
+                throw new IllegalArgumentException("contactDetails is mandatory");
+            }
+            this.name = name;
+            this.lastName = lastName;
+            this.DNI = DNI;
+            this.contactDetails = contactDetails;
+        }
+
+        public Builder idPerson(Integer idPerson){
+            this.idPerson = idPerson;
+            return this; 
+        }
+        public Builder fullName(String fullName){
+            this.fullName = fullName;
+            return this;
+        }
+
+        public PersonaDTO build(){
+            return new PersonaDTO(this);
+        }
+    }
+
+
+    @Override
+    public String toString(){
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+            .append("idPerson",idPerson)
+            .append("name",name)
+            .append("lastName",lastName)
+            .append("fullName",fullName)
+            .append("DNI",DNI)
+            .append("contactDetails",contactDetails)
+            .toString();
+    }
+}
